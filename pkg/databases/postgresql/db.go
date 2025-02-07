3b42a9ecf99970312cc2466c334708bdbc019c82
lovanto <rifkylovanto@gmail.com>
2025-02-07 15:38:35 +0700

    fix: add auto migration

--- pkg/databases/postgresql/db.go
+++ pkg/databases/postgresql/db.go
@@ -1,6 +1,7 @@
 package postgresql
 
 import (
+	"codebase-go-echo/internal/models"
 	"log"
 
 	"gorm.io/driver/postgres"
@@ -16,4 +17,16 @@ func ConnectDB(dsn string) {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
 	log.Println("Database connected successfully")
+
+	AutoMigrate()
+}
+
+func AutoMigrate() {
+	var err error
+	err = DB.AutoMigrate(&models.User{})
+	if err != nil {
+		log.Fatalf("Failed to migrate database: %v", err)
+	}
+
+	log.Println("Database migration completed")
 }
